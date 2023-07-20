35c70e157dfa422d8da2a04bc0030c04018e57cc
Antoine RICHARD <antoine.richard@partner.kiabi.com>
2023-07-20 23:52:52 +0200

    fix: add some jsdoc

--- libs/geometry/lib/vector.js
+++ libs/geometry/lib/vector.js
@@ -1,3 +1,11 @@
+/** @typedef {[number, number]} Position */
+/** @typedef {[number, number]} Vector */
+
+/**
+ * @param {Position} initial
+ * @param {Vector[]} vectors
+ * @returns {Position}
+ */
 export const getPositionFromVectors = (initial, vectors) => {
     return vectors.reduce(([posX, posY], [vX, vY]) => {
         return [posX + vX, posY + vY];
