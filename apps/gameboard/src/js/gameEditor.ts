bd69ae31cf41c0a4edc8ed281f5b4c80408eb66a
Antoine Richard <antoiner.info@gmail.com>
2024-01-02 23:03:50 +0100

    segment preview

--- apps/gameboard/src/js/gameEditor.ts
+++ apps/gameboard/src/js/gameEditor.ts
@@ -12,6 +12,7 @@ class GameEditor {
   canvas: HTMLElement;
   graph: Graph;
   selectedPoint: null | Point;
+  mouse: null | Point;
 
   constructor(viewport: Viewport) {
     this.viewport = viewport;
@@ -21,29 +22,44 @@ class GameEditor {
       [new Segment(p1, p2), new Segment(p2, p3)]
     );
     this.selectedPoint = null;
+    this.mouse = null;
 
     this.#addEventListeners();
   }
 
   #addEventListeners() {
     this.canvas.addEventListener("mousedown", this.#handleMouseDown.bind(this));
+    this.canvas.addEventListener("mousemove", this.#handleMouseMove.bind(this));
+    this.canvas.addEventListener("contextmenu", (event) =>
+      event.preventDefault()
+    );
   }
 
   #handleMouseDown(event: MouseEvent) {
+    event.preventDefault();
     if (event.button == 0) {
       const newPoint = this.viewport.getMouse(event);
-      console.log(newPoint);
       this.graph.addPoint(newPoint);
       if (this.selectedPoint) {
         this.graph.addSegment(new Segment(this.selectedPoint, newPoint));
       }
       this.selectedPoint = newPoint;
+      return;
     }
+
+    if (event.button === 2) {
+      this.selectedPoint = null;
+    }
+  }
+
+  #handleMouseMove(event: MouseEvent) {
+    this.mouse = this.viewport.getMouse(event, true);
   }
 
   render(ctx: CanvasRenderingContext2D) {
     this.graph.render(ctx);
     if (this.selectedPoint) {
+      new Segment(this.selectedPoint, this.mouse).render(ctx, { dash: [3, 3] });
       this.selectedPoint.render(ctx, { outline: true });
     }
   }
